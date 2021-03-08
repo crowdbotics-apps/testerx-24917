e69b1f46d6c17e3396726ab8c1d4c513ea03e5a8
Crowdbotics <team@crowdbotics.com>
2021-03-08 22:05:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/testerx_24917/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testerx_24917;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "testerx_24917";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://testerx-24917.botics.co",
+  defaultTimeout: 5000
+};
